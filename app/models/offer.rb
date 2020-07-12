1082aca2db733e95445597333de9091f7aea23d7
Jony Hayama <jonathan.hayama@gmail.com>
2020-07-12 09:22:26 -0300

    add status logic

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,4 +3,15 @@ class Offer < ApplicationRecord
   validates :url, url: { no_local: true }
   validates_length_of :description, maximum: 500, allow_blank: false
   validates :starts_at, presence: true
+
+  def status
+    (self.is_enabled?) ? 'enabled' : 'disabled'
+  end
+
+  def is_enabled?
+    current_time = Time.now
+    ends_at = (self.ends_at.present?) ? self.ends_at : current_time + 1.day
+    
+    !self.force_disabled && current_time.between?(self.starts_at, ends_at)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_202220) do
+ActiveRecord::Schema.define(version: 2020_07_12_121641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_07_11_202220) do
     t.boolean "premium", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "force_disabled", default: false
   end
 
 end
