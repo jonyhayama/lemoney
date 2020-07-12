eac6ac61728f64f76859b56858486ab0851d7162
Jony Hayama <jonathan.hayama@gmail.com>
2020-07-12 08:24:06 -0300

    add create offer form

--- app/controllers/admin/offers_controller.rb
+++ app/controllers/admin/offers_controller.rb
@@ -3,4 +3,20 @@ class Admin::OffersController < AdminController
   def index
     @offers = Offer.all
   end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.save
+    redirect_to admin_offers_path
+  end
+
+  private 
+
+  def offer_params
+    params.require(:offer).permit(:name, :url, :description, :starts_at, :ends_at)
+  end
 end
